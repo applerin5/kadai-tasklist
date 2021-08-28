5d577be38a894825dcc88cd122ba715fd92e1dfc
apple.rin5 <apple.rin52000@gmail.com>
2021-08-28 21:54:32 +0000

    add column and validation

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,12 +2,18 @@
 
 @section('content')
 
-<!-- ここにページ毎のコンテンツを書く -->
+
+
     <h1>タスク新規作成ページ</h1>
 
     <div class="row">
         <div class="col-6">
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
+            
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
 
                 <div class="form-group">
                     {!! Form::label('content', 'タスク:') !!}
